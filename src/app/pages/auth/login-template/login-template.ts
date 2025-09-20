14817226362e2231b971f4c1a1617ef5f711a6aa
jonatas <contato.dahok@gmail.com>
2025-09-20 15:17:38 -0300

    Featured: Clientes

---
+++ src/app/pages/auth/login-template/login-template.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-template',
+  imports: [LoginForm],
+  templateUrl: './login-template.html',
+  styleUrl: './login-template.scss'
+})
+export class LoginTemplate {
+}
